7b7b204ace081a86b19e5babbdd5ddabdcb11afd
Jenil-Mungalpara <jenilmungalpara44@gmail.com>
2026-07-26 21:10:57 +0530

    social account creation apis

--- server/server.ts
+++ server/server.ts
@@ -4,6 +4,7 @@ import cors from "cors";
 import connectDB from "./config/db.js";
 import dns from "node:dns"; // <-- 1. Import DNS module
 import authRouter from "./routes/auth.route.js";
+import socialAuthRouter from "./routes/socialAuth.route.js";
 
 // <-- 2. Force Node.js to use IPv4 for DNS resolution
 dns.setDefaultResultOrder('ipv4first'); 
@@ -23,6 +24,7 @@ app.get('/', (_req: Request, res: Response) => {
 });
 
 app.use('/api/auth',authRouter)
+app.use('/api/oauth',socialAuthRouter);
 
 //Global error handle
 app.use((err:any,_req:Request,res:Response,_next:NextFunction)=>{
